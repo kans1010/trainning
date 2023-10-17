21adc2eb2c7835182d32196410d9e62b3ba93e58
kans1010 <100337906+kans1010@users.noreply.github.com>
2023-10-17 16:05:40 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -2,14 +2,9 @@ class Solution {
     public int solution(int a, int b, int c) {
         int answer = 0;
         if(a == b && b == c){
-            answer = a * 3 
-                * (int)(Math.pow((double)a,2) * 3) 
-                * (int)(Math.pow((double)a,3) * 3);
+            answer = (int)(a * 3 * (Math.pow(a,2) * 3) * (Math.pow(a,3) * 3));
         }else if(a == b || b == c || a == c){
-            answer = (a + b + c) *  
-                 ((int)(Math.pow((double)a,2))
-                + (int)(Math.pow((double)b,2))
-                + (int)(Math.pow((double)c,2)));
+            answer = (int)((a + b + c) * (Math.pow(a,2) + Math.pow(b,2)+ Math.pow(c,2)));
         }else{
             answer = a + b + c;
         }
