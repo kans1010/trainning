51c0e01ee34438c800f5fed87cbc8e0c211270ef
kans1010 <100337906+kans1010@users.noreply.github.com>
2023-10-25 08:23:06 +0900

    [level unrated] Title: 배열 만들기 5, Time: 3.96 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < intStrs.length; i++){
+            if(k < Integer.parseInt(intStrs[i].substring(s,s+l))){
+                list.add(Integer.parseInt(intStrs[i].substring(s,s+l)));
+            }
+        }
+         int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i).intValue();
+        }
+        return answer;
+    }
+}
