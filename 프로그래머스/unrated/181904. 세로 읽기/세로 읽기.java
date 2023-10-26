bae06c024a3c9b2d8bc449210521c301bec2354e
kans1010 <100337906+kans1010@users.noreply.github.com>
2023-10-26 17:17:40 +0900

    [level unrated] Title: 세로 읽기, Time: 9.36 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -2,10 +2,8 @@ class Solution {
     public String solution(String my_string, int m, int c) {
         char[] arr = my_string.toCharArray();
         String answer = "";
-        for(int i = 0; i < arr.length; i++){
-            if(i%m == c-1){
-                answer +=arr[i];
-            }
+        for(int i = c-1; i < arr.length; i+=m){
+            answer +=arr[i];
         }
         return answer;
     }
