bc9a80c59f0cb1e1efaf8e544e3640fa938ae90b
kans1010 <100337906+kans1010@users.noreply.github.com>
2023-10-30 12:25:28 +0900

    [level unrated] Title: 배열 만들기 3, Time: 0.18 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,12 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i = 0; i<intervals.length;i++){
-            for(int j = intervals[i][0];j<=intervals[i][1];j++)list.add(arr[j]);
+        int x = (intervals[0][1]-intervals[0][0]+1)+
+            (intervals[1][1]-intervals[1][0]+1);
+        int[] answer = new int[x];
+        int j = 0;
+        for(int i = intervals[0][0]; i<=intervals[0][1];i++){
+            answer[j] = arr[i]; j++;
+        }for(int i = intervals[1][0]; i<=intervals[1][1];i++){
+            answer[j] = arr[i]; j++;
         }
-        return list.stream().mapToInt(x->x).toArray();
+        
+        return answer;
     }
 }
