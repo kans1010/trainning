5d4ff919bd4e9ade558123bc87393c6226cf066c
kans1010 <100337906+kans1010@users.noreply.github.com>
2023-10-30 13:00:26 +0900

    [level unrated] Title: 2의 영역, Time: 1.69 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -1,19 +1,18 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr) {
-        int min = 100000, max = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 2) {
-                min = Math.min(min, i);
-                max = Math.max(max, i);
+        int[] answer = {};
+        int s = Integer.MAX_VALUE; int e = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length;i++){
+            if(arr[i] == 2){
+                s = Math.min(s,i);
+                e = Math.max(e,i);
             }
         }
-
-        if (min <= max) {
-            return Arrays.copyOfRange(arr, min, max + 1);
-        } else {
-            return new int[]{-1};
-        }
+        if(s<=e){
+            answer = Arrays.copyOfRange(arr,s,e+1);
+        }else{ return new int[] {-1};}
+        return answer;
     }
 }
