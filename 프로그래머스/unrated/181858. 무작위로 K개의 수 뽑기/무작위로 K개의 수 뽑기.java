9759bd2fa82d1d242c302fe9748d3d7c160cdf2f
kans1010 <100337906+kans1010@users.noreply.github.com>
2023-11-12 17:05:04 +0900

    [level unrated] Title: 무작위로 K개의 수 뽑기, Time: 12.38 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[k];
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i<arr.length; i++){
+            if(list.size()==k) break;
+            if(!list.contains(arr[i])) list.add(arr[i]);
+        }
+        while(list.size()<k){
+            list.add(-1);
+        }
+        return list.stream().mapToInt(x->x).toArray();
+    }
+}
